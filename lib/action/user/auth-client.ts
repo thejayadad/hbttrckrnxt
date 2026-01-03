8e48c6b481a9b350788fc2bbe29b828bfa9380f2
thejayadad <jaydunb12@gmail.com>
2026-01-03 05:46:25 -0500

    update

--- lib/action/user/auth-client.ts
+++ lib/action/user/auth-client.ts
@@ -3,5 +3,7 @@ import { createAuthClient } from "better-auth/client";
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_APP_URL
 })
+
+export const {signIn, signOut, useSession} = authClient
